923ab1ce0432e5cd5ffc8a5800ba9e6a42eb298f
Caio Carvalho <kaiogesteira@gmail.com>
2023-04-17 15:19:47 -0300

    Post Messages

--- src/app.js
+++ src/app.js
@@ -73,14 +73,70 @@ app.post("/participants", async (req, res) => {
 });
 
 // Get Participants route
-app.get('/participants', async (req, res) => {
+app.get("/participants", async (req, res) => {
   try {
-    const activeParticipants = await db.collection("participants").find().toArray();
+    const activeParticipants = await db
+      .collection("participants")
+      .find()
+      .toArray();
     return res.send(activeParticipants);
   } catch (err) {
     res.status(500).send(err.message);
   }
 });
 
+// Post participants messages route
+app.post("/messages", async (req, res) => {
+  const { to, text, type } = req.body;
+  const { user } = req.headers;
+
+  // Message Schema
+  const messageSchema = joi.object({
+    to: joi.string().required().min(1),
+    text: joi.string().required().min(1),
+    type: joi.string().required().valid("message", "private_message"),
+  });
+
+  // Validations
+  // verifying if the user exists in the database
+  try {
+    const fromUser = await db
+      .collection("participants")
+      .findOne({ name: user });
+    if (!fromUser)
+      return res
+        .status(422)
+        .send("User doesn't exist on database or it's not logged");
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+
+  const messageValidation = messageSchema.validate(req.body, {
+    abortEarly: false,
+  });
+  if (messageValidation.error) {
+    const errors = messageValidation.error.details.map(
+      (detail) => detail.message
+    );
+    return res.status(422).send(errors);
+  }
+
+  // Message Object
+  const messageObject = {
+    from: user,
+    to,
+    text,
+    type,
+    time: dayjs().format("HH:mm:ss"),
+  };
+
+  // Post the message to the database
+  try {
+    await db.collection("messages").insertOne(messageObject);
+    return res.sendStatus(201);
+  } catch (err) {
+    return res.status(500).send(err.message);
+  }
+});
 
 app.listen(PORT, () => console.log(`Running on port ${PORT}`));
